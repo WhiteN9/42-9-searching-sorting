885ce3bb407fd816cc10b34456902434262f38b3
White Nguyen <whitenguyen0909@gmail.com>
2022-07-10 18:07:54 -0600

    search by email and search by name is now functional

--- src/searchByEmail.js
+++ src/searchByEmail.js
@@ -1,9 +1,44 @@
+const {
+  data: { user },
+} = require("./data.js");
 /**
  * Return the index of the customer with given email, otherwise -1.
  * @param {string} email The email address to search for
  * @param {*} customers The array to search
  */
- function searchByEmail(email, customers) {}
+function searchByEmail(email, customers) {
+  if (!customers) {
+    return -1;
+  }
+  let lowerIndex = 0;
+  let upperIndex = customers.length;
+  let index;
+  while (lowerIndex <= upperIndex) {
+    index = Math.floor((lowerIndex + upperIndex) / 2);
+    //Compare variable email against targeted email
+    let comparison = email.localeCompare(customers[index].email);
+    // Returns 0 if they are equivalent
+    if (comparison === 0) {
+      return index;
+    }
+    // Positive when the referenceStr occurs after compareString
+    // Positive when the reference email occurs after the email at that index
+    else if (comparison > 0) {
+      lowerIndex = index + 1;
+    }
+    // Negative when the referenceStr occurs before compareString
+    // Negative when the reference email occurs before the email at that index
+    else if (comparison < 0) {
+      upperIndex = index - 1;
+    }
+  }
 
- module.exports = searchByEmail;
- 
+  return -1;
+}
+
+const customers = [...user].sort((a, b) => a.email.localeCompare(b.email));
+
+const result = searchByEmail("Roma_Halvorson@yahoo.com", customers);
+console.log(result);
+
+module.exports = searchByEmail;
--- src/searchByName.js
+++ src/searchByName.js
@@ -1,10 +1,37 @@
-
+// const {
+//   data: { user },
+// } = require("./data.js");
 /**
  * Use a binary search to find the customer with given name
  * @param {string} firstName
  * @param {string} lastName
  * @param {array} customers
  */
-function searchByName(firstName, lastName, customers) {}
+function searchByName(firstName, lastName, customers) {
+  if (!customers) {
+    return -1;
+  }
+  let lowerIndex = 0;
+  let upperIndex = customers.length - 1;
+  let index;
+  while (lowerIndex <= upperIndex) {
+    index = Math.floor((lowerIndex + upperIndex) / 2);
+    let comparison = customers[index].lastName.localeCompare(lastName);
+    if (comparison === 0) {
+      return index;
+    } else if (comparison > 0) {
+      upperIndex = index - 1;
+    } else if (comparison < 0) {
+      lowerIndex = index + 1;
+    }
+  }
+  return -1;
+}
 
+// const customers = [...user]
+//   .sort((a, b) => a.firstName.localeCompare(b.firstName))
+//   .sort((a, b) => a.lastName.localeCompare(b.lastName));
+
+// const result = searchByName("Fred", "Flintstone", customers);
+// console.log(result);
 module.exports = searchByName;
